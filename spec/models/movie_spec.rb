088f9a35a055d4cc0747e541efd0bd173edcfee7
Clay Stewart <claytonmstewart@gmail.com>
2013-10-10 10:03:12 -0500

    Add passing tests to rspec for movie model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:movie) { Movie.new }
+
+  describe "#snippet" do
+    context "when description is less than or equal to 50 characters" do 
+      it "returns the full description" do
+
+        allow(movie).to receive(:description).and_return('asdfasdfljkasdfljkasdf')
+
+        expect(movie.snippet).to eq('asdfasdfljkasdfljkasdf')
+      end
+    end
+
+    context "when description is greater than 50 characters" do
+      it "returns the first 47 characters and appends '...'" do
+
+        allow(movie).to receive(:description).and_return('An offbeat romantic comedy about a woman who doasdfasdfadsfadsfaafdsafdasfasdfasfs')
+
+        expect(movie.snippet).to eq('An offbeat romantic comedy about a woman who do...')
+      end
+    end
+  end
+
+
 end
